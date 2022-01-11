c141f646f044094aab02b58320699d72dadb5fd9
LAPTOP-1SGCLO1T\Fisher <unifisher02@gmail.com>
2022-01-12 07:28:13 +0800

    Jwt Login

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const registerValidation = require("../validation").registerValidation;
 const loginValidation = require("../validation").loginValidation;
 const User = require("../models").userModel;
+const jwt = require("jsonwebtoken");
 
 router.use((req, res, next) => {
   console.log("A request is coming in to auth.js");
@@ -15,7 +16,7 @@ router.get("/testAPI", (req, res) => {
   return res.json(msgObj);
 });
 
-router.post("/register",async (req, res) => {
+router.post("/register", async (req, res) => {
   //check the Validation of data
   const { error } = registerValidation(req.body); //value 和 error(成功則無)
   if (error) return res.status(400).send(error.details[0].message);
@@ -24,25 +25,54 @@ router.post("/register",async (req, res) => {
   const emailExist = await User.findOne({ email: req.body.email });
   if (emailExist)
     return res.status(400).send("Email has already been registered.");
-  
+
   //
   const newUser = new User({
-    email:req.body.email,
-    username:req.body.username,
-    password:req.body.password,
-    role:req.body.role,
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password,
+    role: req.body.role,
   });
 
-  try{
+  try {
     const savedUser = await newUser.save();
     res.status(200).send({
-      msg:"success",
+      msg: "success",
       saveObject: savedUser,
     });
-  }catch (err){
+  } catch (err) {
     res.status(400).send("User not saved");
     console.log(err.message);
   }
+});
+
+router.post("/login", (req, res) => {
+  //check the Validation of data
+  const { error } = loginValidation(req.body); //value 和 error(成功則無)
+  if (error) return res.status(400).send(error.details[0].message);
+
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) {
+      res.status(400).send(err);
+    }
+    if (!user) {
+      res.status(401).send("User not found.");
+    } else {
+      user.comparePassword(req.body.password, function (err, isMatch) {
+        if (error) return res.status(400).send(err);
+
+        if (isMatch) {
+          //JWT Create
+          const tokenObject = { _id: user._id, email: user.email };
+          const token = jwt.sign(tokenObject, process.env.PASSPORT_SECRET);
+          res.send({ success: true, token: "JWT " + token, user });
+        } else {
+          console.log(er);
+          res.status(401).send("Wrong password");
+        }
+      });
+    }
+  });
 });
 
 module.exports = router;
